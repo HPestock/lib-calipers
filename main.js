bcafb8bd46aaf4f354b466a30b1696084310421d
HPestock <pestock32@gmail.com>
2025-08-09 01:04:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,8 +13,12 @@ Graphics.fill([250,20,35]);
 Graphics.background();    
 
 var X = 0;
+var SPEED = 2;
+
+var inprompt = new PromptStream();
 
 function frameUpdate(){
+    for(let i=0;i<2*SPEED;i++){
     //x += 2*50*Math.random()/DeltaTime.GetDeltaTime();
     //y += 2*50*Math.random()/DeltaTime.GetDeltaTime();
     let r = Math.random()*2*Math.PI;
@@ -29,8 +33,14 @@ function frameUpdate(){
     Graphics.text('Graphics is working.',[Graphics.width/2,Graphics.height/2]);
     u = (-DeltaTime.GetDeltaTime()*0.01 + 1) * u;
     v = (-DeltaTime.GetDeltaTime()*0.01 + 1) * v;
+    }
 
     DeltaTime.Update(); //keep track of time delta
+
+    //Teletype.out_sw(["",Keyboard.stdflush(),""]);
+    inprompt.Update();
+    Teletype.set_post(inprompt.file);
+    Teletype.set_curoff(inprompt.curoff);
 }
 
 setInterval(frameUpdate, 1000/DeltaTime.framerate); //run frameUpdate (DeltaTime.framerate === 60) times per second (1000 milliseconds)
